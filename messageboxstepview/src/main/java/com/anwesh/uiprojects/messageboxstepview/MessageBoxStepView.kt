a734308ac8d52465887915947f818a1ba14c266a
Anwesh43 <anweshthecool0@gmail.com>
2018-09-26 02:31:29 +0530

    importing libraries and defining nodes

--- messageboxstepview/src/main/java/com/anwesh/uiprojects/messageboxstepview/MessageBoxStepView.kt
+++ messageboxstepview/src/main/java/com/anwesh/uiprojects/messageboxstepview/MessageBoxStepView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.messageboxstepview
 /**
  * Created by anweshmishra on 26/09/18.
  */
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.Color
+import android.view.MotionEvent
+import android.view.View
+
+val nodes : Int = 5
