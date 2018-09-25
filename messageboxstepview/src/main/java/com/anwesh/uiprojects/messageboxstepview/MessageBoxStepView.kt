0585ed581b79cbd732766c01d8729b183751109c
Anwesh43 <anweshthecool0@gmail.com>
2018-09-26 02:33:30 +0530

    adding MessageBoxStepView class

--- messageboxstepview/src/main/java/com/anwesh/uiprojects/messageboxstepview/MessageBoxStepView.kt
+++ messageboxstepview/src/main/java/com/anwesh/uiprojects/messageboxstepview/MessageBoxStepView.kt
@@ -13,3 +13,21 @@ import android.view.MotionEvent
 import android.view.View
 
 val nodes : Int = 5
+
+class MessageBoxStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
