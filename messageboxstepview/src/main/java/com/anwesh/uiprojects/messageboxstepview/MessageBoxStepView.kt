3dee03bffceea6db0167917301098ee7fbe722e4
Anwesh43 <anweshthecool0@gmail.com>
2018-09-26 02:59:11 +0530

    adding MBSNoe class

--- messageboxstepview/src/main/java/com/anwesh/uiprojects/messageboxstepview/MessageBoxStepView.kt
+++ messageboxstepview/src/main/java/com/anwesh/uiprojects/messageboxstepview/MessageBoxStepView.kt
@@ -103,4 +103,48 @@ class MessageBoxStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MBSNode(var i : Int, val state : State = State()) {
+
+        private var next : MBSNode? = null
+        private var prev : MBSNode? = null
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = MBSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        init {
+            addNeighbor()
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMBSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MBSNode {
+            var curr : MBSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
