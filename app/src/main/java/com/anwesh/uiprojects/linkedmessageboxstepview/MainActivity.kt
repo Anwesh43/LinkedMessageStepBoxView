c1c3249839df52825e4ed86853a6ff177f7ede4e
Anwesh43 <anweshthecool0@gmail.com>
2018-09-26 03:14:15 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedmessageboxstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmessageboxstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedmessageboxstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.messageboxstepview.MessageBoxStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MessageBoxStepView.create(this)
     }
 }
